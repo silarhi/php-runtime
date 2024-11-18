5772fc968eb02a41675f6592be7bf6d14af62189
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-11-18 11:11:01 +0100

    add response runner in frankenphp (#177)

--- src/frankenphp-symfony/tests/RuntimeTest.php
+++ src/frankenphp-symfony/tests/RuntimeTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Runtime\FrankenPhpSymfony\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Runtime\FrankenPhpSymfony\ResponseRunner;
 use Runtime\FrankenPhpSymfony\Runner;
 use Runtime\FrankenPhpSymfony\Runtime;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
@@ -14,6 +16,12 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
  */
 final class RuntimeTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        unset($_SERVER['FRANKENPHP_WORKER']);
+    }
+
     public function testGetRunner(): void
     {
         $application = $this->createStub(HttpKernelInterface::class);
@@ -24,5 +32,17 @@ final class RuntimeTest extends TestCase
 
         $_SERVER['FRANKENPHP_WORKER'] = 1;
         $this->assertInstanceOf(Runner::class, $runtime->getRunner($application));
+    }
+
+    public function testGetResponseRunner(): void
+    {
+        $application = $this->createStub(Response::class);
+
+        $runtime = new Runtime();
+        $this->assertNotInstanceOf(ResponseRunner::class, $runtime->getRunner(null));
+        $this->assertNotInstanceOf(ResponseRunner::class, $runtime->getRunner($application));
+
+        $_SERVER['FRANKENPHP_WORKER'] = 1;
+        $this->assertInstanceOf(ResponseRunner::class, $runtime->getRunner($application));
     }
 }
